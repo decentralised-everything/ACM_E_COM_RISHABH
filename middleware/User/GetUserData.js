a445782f91f7f42fdff042ab2ac79e1f3857cc7a
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-04-14 16:55:35 +0530

    ...

--- middleware/User/GetUserData.js
+++ middleware/User/GetUserData.js
@@ -1,13 +1,21 @@
 const Users = require("../../models/Users");
-const GetUser = async (req, res) => {
+const GetUserData = async (req, res, next) => {
   //
   // using "email" as a token to find the user from the database since "email" is unique
-  try {
-    const user = await Users.findOne({ email: res.locals.user.email });
-    res.json(user);
-  } catch (error) {
-    res.status(500).send(error); // gosh
-  }
+  const { email } = req.params;
+  await Users.findOne({ email: email }, (err, userData) => {
+    if (err) {
+      return res.status(403).send(err);
+    } else {
+      // "json stuff" to remove specific data extracted from database that shouldn't be displayed
+      delete userData.password;
+      delete userData._id;
+
+      // displaying user (data) without "id" and "password"
+      res.json(userData);
+      next();
+    }
+  });
 };
 
-module.exports = GetUser;
+module.exports = GetUserData;
