ae8a3e1492d11e1340a2716fb25164d5611e2cbc
Rishabh Barnwal <f20201677@pilani.bits-pilani.ac.in>
2021-04-14 17:20:54 +0530

    ...

--- middleware/User/GetUserData.js
+++ middleware/User/GetUserData.js
@@ -8,12 +8,14 @@ const GetUserData = async (req, res, next) => {
       return res.status(403).send(err);
       // either the data was corrupted or name entered didn't exist (or idk)
     } else {
+      // first we need to convert the data to JSON, then only "json stuff" can be applied, gosh
+      userData = JSON.parse(JSON.stringify(userData));
       // "json stuff" to remove specific data extracted from database that shouldn't be displayed
       delete userData.password;
       delete userData._id;
 
       // displaying user (data) without "id" and "password"
-      res.json(userData);
+      res.send(userData);
       next();
     }
   });
